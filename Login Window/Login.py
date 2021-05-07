bd4ff37a375abedcbadd5561110ee07fb37b8519
codemeatloaf <65986665+codemeatloaf@users.noreply.github.com>
2021-05-07 13:21:01 +1000

    commit on main

--- Login Window/Login.py
+++ Login Window/Login.py
@@ -1,13 +1,11 @@
 # imports
 import tkinter
+import sqlite3
 
 # defs
 
 # window
-tkWindow = tk() 
-
-
-
-
-
-tkWindow.mainloop() 
+tkmain=tkinter.Tk()
+tkmain.title('Login / Registery')
+tkmain.geometry('400x300')
+tkmain.mainloop()
