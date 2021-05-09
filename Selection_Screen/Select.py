247f0659e37424f3e5503e5564e932cf7841673b
codemeatloaf <65986665+codemeatloaf@users.noreply.github.com>
2021-05-09 19:34:51 +1000

    commit on main

--- Selection_Screen/Select.py
+++ Selection_Screen/Select.py
@@ -9,13 +9,16 @@ import sys
 # window
 tkmain = tkinter.Tk()
 
-# fonts
-info_font=tkFont.Font(family="Source Code Pro Bold", size=20)
-input_font=tkFont.Font(family="Source Code Pro Italic", size=15)
 
 # title and geometry
 tkmain.title('Selection')
 tkmain.geometry('320x190')
+
+# fonts
+info_font=tkFont.Font(family="Source Code Pro Bold", size=20)
+input_font=tkFont.Font(family="Source Code Pro Italic", size=15)
+
+
 
 # end of mainloop
 tkmain.mainloop()
