bccf272a1b8e07181e7376e3bfa582eae440c7b0
codemeatloaf <65986665+codemeatloaf@users.noreply.github.com>
2021-05-10 17:16:01 +1000

    commmit on main

--- Login_Window/Register.py
+++ Login_Window/Register.py
@@ -10,9 +10,14 @@ import sqlite3
 def finish():
     conn = sqlite3.connect('login.sqlite')
     c = conn.cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS staff (username TEXT, password TEXT)")
+    c.execute("CREATE TABLE IF NOT EXISTS staff (username TEXT, password TEXT, level INTEGER)")
     conn.commit()
     conn.close()
+
+option_list = [
+    "Admin",
+    "Staff"
+]
 
 # window
 tk_main = tk.Tk()
@@ -21,7 +26,7 @@ tk_main['padx']=padding
 
 # name and geometry
 tk_main.title('Create Account')
-tk_main.geometry('270x200')
+tk_main.geometry('270x260')
 
 # fonts
 info_font=tkFont.Font(family="Source Code Pro Bold", size=20)
@@ -36,8 +41,8 @@ username = Label(tk_main, text="Username", font=input_font)
 username.grid(row=2, column=0)
 password = Label(tk_main, text="Password", font=input_font)
 password.grid(row=4, column=0)
-#level = Label(tk_main, text="Level", font=input_font)
-#level.grid(row=6, column=0)
+level = Label(tk_main, text="Level", font=input_font)
+level.grid(row=7, column=0)
 
 
 # username input 
@@ -51,38 +56,48 @@ password_storage = Entry(tk_main, textvariable=password_entry)
 password_storage.grid(row=6, column=0)
 
 # level input
-#level_entry = tk.StringVar()
-#level_storage = Entry(tk_main, textvariable=level_entry)
-#password_storage.grid(row=7, column=0)
+level_entry = tk.StringVar()
+level_entry.set('Choose Level')
+level_storage = tk.OptionMenu(tk_main, level_entry, *option_list)
+level_storage.grid(row=8, column=0)
+
+
 
 # insert info into db
 def savedata():
     conn = sqlite3.connect('login.sqlite')
     c = conn.cursor()
-    c.execute('INSERT INTO staff (username, password) VALUES (?,?)', (username_entry.get(), password_entry.get()))
+    c.execute('INSERT INTO staff (username, password, level) VALUES (?,?,?)', (username_entry.get(), password_entry.get(), level_entry.get()))
     conn.commit()
     print("OK")
 
 # security
 def blank1():
-    if password_storage.get():
+    if username_storage.get():
         blank2()
     else:
-        messagebox.showinfo('info', 'Input required.')
-        
+        messagebox.showinfo('info', 'Username required.')
+
 def blank2():
-    if username_storage.get():
+    if password_storage.get():
+        blank3()
+    else:
+        messagebox.showinfo('info', 'Password required.')      
+
+def blank3():
+    if level_entry.get():
         savedata()
     else:
-        messagebox.showinfo('info', 'Input required.')
+        messagebox.showinfo('info', 'Level required.')
+
 
 # spacer
 spacer=tk.Label(tk_main, text=' ')
-spacer.grid(row=8, column=0)
+spacer.grid(row=9, column=0)
 
 # save button
 enter_btn = Button(text="Enter",command=blank1)
-enter_btn.grid(row=9, column=0)
+enter_btn.grid(row=10, column=0)
 
 # end mainloop
 finish()
