542a23e7fd9e739f30129ef03addbb63cd112122
codemeatloaf <65986665+codemeatloaf@users.noreply.github.com>
2021-05-25 09:00:41 +1000

    commit on main

--- Selection_Screen/Select.py
+++ Selection_Screen/Select.py
@@ -1,24 +1,34 @@
 # imports
-import tkinter
-import sqlite3
-import tkinter.font as tkFont
+import random
+import tkinter as tk
+from tkinter import *
 from tkinter import messagebox
-import os
+import tkinter.font as tkFont
+import sqlite3
 import sys
+import os
 
 # window
-tkmain = tkinter.Tk()
+tk_main = tk.Tk()
 
 
 # title and geometry
-tkmain.title('Selection')
-tkmain.geometry('320x100')
+tk_main.title('Selection')
+tk_main.geometry('320x100')
 
 # fonts
 info_font=tkFont.Font(family="Source Code Pro Bold", size=20)
 input_font=tkFont.Font(family="Source Code Pro Italic", size=15)
 
+def data_window():
+    tk_main.destroy()
+    os.system(r'python Login_Window\Register.py')
+
+def reg_window():
+    tk_main.destroy()
+    os.system(r'python Database_Interface\Interface.py')
+
 
 
 # end of mainloop
-tkmain.mainloop() 
+tk_main.mainloop() 
