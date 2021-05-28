fdfc6f20c85121f34dfec4aa58fe85949ee02167
codemeatloaf <65986665+codemeatloaf@users.noreply.github.com>
2021-05-28 10:32:20 +1000

    commit on main

--- Selection_Screen/Select.py
+++ Selection_Screen/Select.py
@@ -3,6 +3,7 @@ import random
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
+import tkinter
 import tkinter.font as tkFont
 import sqlite3
 import sys
@@ -11,24 +12,39 @@ import os
 # window
 tk_main = tk.Tk()
 
-
 # title and geometry
 tk_main.title('Selection')
-tk_main.geometry('320x100')
+tk_main.geometry('295x100')
+
 
 # fonts
 info_font=tkFont.Font(family="Source Code Pro Bold", size=20)
 input_font=tkFont.Font(family="Source Code Pro Italic", size=15)
+input_font=tkFont.Font(family="Source Code Pro Bold", size=9)
 
-def data_window():
+# definitions
+def reg_window():
     tk_main.destroy()
     os.system(r'python Login_Window\Register.py')
 
-def reg_window():
+def data_window():
     tk_main.destroy()
     os.system(r'python Database_Interface\Interface.py')
 
+# buttons
+data_btn = Button(text="Edit Data",command=data_window)
+data_btn.place(x=25, y=50)
+data_btn.config(height = 2, width = 9)
+data_btn['font'] = input_font
 
+reg_btn = Button(text="Register",command=reg_window)
+reg_btn.place(x=205, y=50)
+reg_btn.config(height = 2, width = 8)
+reg_btn['font'] = input_font
+
+# Labels
+info_label = Label(tk_main, text='Select', font=info_font)
+info_label.place(x=100, y=3)
 
 # end of mainloop
 tk_main.mainloop() 
